724d8c592f8db21822f6458f1e20ccb3076f3320
xhronpe00 <petr.hron04@gmail.com>
2025-02-25 19:13:10 +0100

    Comment out auth state logic code, moved to start state

--- src/state_auth.c
+++ src/state_auth.c
@@ -6,27 +6,27 @@
 
 void state_auth_logic(cmd_ptr cmd)
 {
-    if (cmd == cmd_auth){
-        cmd();
-    }
-    else if (cmd == cmd_join) {
-        printf("ERROR: trying to join channel while not authenticated\n");
-        return;
-    }
-    else if (cmd == cmd_rename) {
-        printf("ERROR: trying to rename while not authenticated\n");
-        return;
-    }
-    else if (cmd == cmd_help) {
-        cmd();
-        return;
-    }
-    else if (cmd == cmd_msg) {
-        printf("ERROR: trying to send message while not authenticated\n");
-        return;
-    }
-    else {
-        printf("ERROR: unknown input\n");
-        return;
-    }
+    // if (cmd == cmd_auth){
+    //     cmd();
+    // }
+    // else if (cmd == cmd_join) {
+    //     printf("ERROR: trying to join channel while not authenticated\n");
+    //     return;
+    // }
+    // else if (cmd == cmd_rename) {
+    //     printf("ERROR: trying to rename while not authenticated\n");
+    //     return;
+    // }
+    // else if (cmd == cmd_help) {
+    //     cmd();
+    //     return;
+    // }
+    // else if (cmd == cmd_msg) {
+    //     printf("ERROR: trying to send message while not authenticated\n");
+    //     return;
+    // }
+    // else {
+    //     printf("ERROR: unknown input\n");
+    //     return;
+    // }
 }
