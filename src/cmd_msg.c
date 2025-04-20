1b729e324332eb5d49a99892e397a069bd9ca69c
xhronpe00 <petr.hron04@gmail.com>
2025-04-20 05:14:49 +0200

    Implement message command

--- src/cmd_msg.c
+++ src/cmd_msg.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "./commands.h"
 #include "./debug.h"
+#include "./global.h"
+#include "./network.h"
+#include "./maximums.h"
+#include "./messages.h"
+#include "./serialize.h"
 
 void cmd_msg(void)
 {
-    printf_debug_simple(COLOR_INFO, "in cmd_msg function");
+    printf_debug_simple(COLOR_INFO, "executing cmd_msg");
+
+    char message[MAX_MESSAGE_CONTENT_LEN] = {0};
+    memcpy(message, unprocessed_line, MAX_MESSAGE_CONTENT_LEN);
+
+    struct MSG msg;
+    create_msg(&msg, confirmed_msg_ids_index, display_name, message);
+    size_t buffer_size;
+    uint8_t *in_buffer = serialize_msg(&msg, &buffer_size);
+
+    send_network_msg_udp(in_buffer, buffer_size);
 }
