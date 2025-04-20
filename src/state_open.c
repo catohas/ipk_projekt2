2da554836a499eae19a702c1386d7ba19c350aef
xhronpe00 <petr.hron04@gmail.com>
2025-04-20 05:30:09 +0200

    Fix state open logic

--- src/state_open.c
+++ src/state_open.c
@@ -1,47 +1,55 @@
-void handle_confirm_msg_state_open(unsigned char *buffer, int length)
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "./commands.h"
+#include "./debug.h"
+#include "./serialize.h"
+#include "./deserialize.h"
+#include "./global.h"
+#include "./network.h"
+#include "./messages.h"
+#include "./state.h"
+
+void state_open_logic(cmd_ptr cmd)
 {
-    (void)buffer;
-    (void)length;
+    if (cmd == cmd_auth){
+        printf("ERROR: already authenticated\n");
+    }
+    else if (cmd == cmd_join) {
+        cmd();
+    }
+    else if (cmd == cmd_rename) {
+        printf_debug_simple(COLOR_INFO, "renaming...");
+        cmd();
+    }
+    else if (cmd == cmd_help) {
+        cmd();
+    }
+    else if (cmd == cmd_msg) {
+        cmd();
+    }
+    else {
+        printf("ERROR: unknown input\n");
+    }
 }
 
 void handle_reply_msg_state_open(unsigned char *buffer, int length)
 {
-    (void)buffer;
-    (void)length;
-}
+    printf_debug_simple(COLOR_SUCCESS, "handling reply message, state: OPEN");
 
-void handle_auth_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_join_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_ping_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_err_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_bye_msg_state_open(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
+    struct Reply_MSG *reply_msg = deserialize_reply_msg(buffer, length);
+    if (reply_msg->result == 1) {
+        printf("Action Success: %s\n", reply_msg->message_contents);
+        printf_debug_simple(COLOR_SUCCESS, "transitioning to state END");
+        state = STATE_END;
+    }
+    else if (reply_msg->result == 0) {
+        printf("Action Failure: %s\n", reply_msg->message_contents);
+        printf_debug_simple(COLOR_SUCCESS, "transitioning to state END");
+        state = STATE_END;
+    }
+    else {
+        // malformed message ig
+    }
+    free_reply_msg(reply_msg);
 }
