4b7cb5a01a6446ee996f529c8cadcaec98efd414
xhronpe00 <petr.hron04@gmail.com>
2025-04-20 05:17:16 +0200

    Remove unnecessary deserialization functions

--- src/deserialize.h
+++ src/deserialize.h
@@ -17,8 +17,6 @@ extern "C" {
 
 struct Confirm_MSG *deserialize_confirm_msg(uint8_t *buffer, const size_t buffer_size);
 struct Reply_MSG *deserialize_reply_msg(uint8_t *buffer, const size_t buffer_size);
-struct Auth_MSG *deserialize_auth_msg(uint8_t *buffer, const size_t buffer_size);
-struct Join_MSG *deserialize_join_msg(uint8_t *buffer, const size_t buffer_size);
 struct MSG *deserialize_msg(uint8_t *buffer, const size_t buffer_size);
 struct Err_MSG *deserialize_err_msg(uint8_t *buffer, const size_t buffer_size);
 struct Bye_MSG *deserialize_bye_msg(uint8_t *buffer, const size_t buffer_size);
