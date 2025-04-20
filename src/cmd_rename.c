648a57878837553e3feb0bf4d61a4171c134d8e1
xhronpe00 <petr.hron04@gmail.com>
2025-04-20 05:15:18 +0200

    Implement rename command

--- src/cmd_rename.c
+++ src/cmd_rename.c
@@ -1,6 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "./commands.h"
+#include "./debug.h"
+#include "./maximums.h"
+#include "./global.h"
 
 void cmd_rename(void)
 {
-    return;
+    printf_debug_simple(COLOR_INFO, "executing cmd_rename");
+
+    line = strtok(NULL, " "); // get rid of /rename
+    if (line == NULL) {
+        fprintf(stderr, "Please provide 'display_name'\n");
+        return;
+    }
+    memcpy(display_name, line, MAX_DISPLAY_NAME_LEN);
+
+    printf_debug(COLOR_SUCCESS, "changed display name to %s", display_name);
 }
