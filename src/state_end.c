c3d9b1aea4aafc11d2c745dffb9fdef26796f437
xhronpe00 <petr.hron04@gmail.com>
2025-04-20 05:29:38 +0200

    Remove unnecessary functions from state end

--- src/state_end.c
+++ src/state_end.c
@@ -1,46 +1,10 @@
-void handle_confirm_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
+#include <stdint.h>
+
+#include "./debug.h"
+#include "./deserialize.h"
+#include "./messages.h"
 
 void handle_reply_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_auth_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_join_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_ping_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_err_msg_state_end(unsigned char *buffer, int length)
-{
-    (void)buffer;
-    (void)length;
-}
-
-void handle_bye_msg_state_end(unsigned char *buffer, int length)
 {
     (void)buffer;
     (void)length;
